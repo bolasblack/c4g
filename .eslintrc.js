ca90de2f4ba060bd6ccab3ed86e3939c512a1662
c4605 <bolasblack@gmail.com>
2019-09-02 12:13:41 +0800

    feat(node-new): generate index.spec.ts when selected jest in proj-tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,7 @@ module.exports = {
   extends: [
     './node_modules/@c4605/toolconfs/eslintrc.base',
     './node_modules/@c4605/toolconfs/eslintrc.prettier',
-    './node_modules/@c4605/toolconfs/eslintrc.ts',
+    './node_modules/@c4605/toolconfs/eslintrc.esnext',
   ],
-  parserOptions: {
-    project: './tsconfig.json',
-  },
   rules: {},
 }
---
+++ src/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['../node_modules/@c4605/toolconfs/eslintrc.ts'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {},
+}
